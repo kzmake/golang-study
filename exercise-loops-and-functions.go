de09628f10d48638749d8cc092f40e0d0a842f53
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-09 23:10:47 +0900

    Modify for loop 10.times to z_n - z_(n-1) < 1e-10 == true

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,18 +5,29 @@ import (
 	"math"
 )
 
+func isChecked(a float64, b float64) bool {
+	if math.Abs(a-b) < 1e-10 {
+		return true
+	}
+
+	return false
+}
+
 func Sqrt(x float64) float64 {
 	var z float64 = 1.0
 
-	for i := 0; i < 10; i++ {
+	for {
+		_z := z
 		z = z - 0.5*(z*z-x)/z
-	}
 
-	return z
+		if isChecked(z, _z) {
+			return z
+		}
+	}
 }
 
 func main() {
 	fmt.Println("math.Sqrt(2) :\t", math.Sqrt(2))
 	fmt.Println("Sqrt(2)      :\t", Sqrt(2))
-	fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", Sqrt(2) - math.Sqrt(2))
+	fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", Sqrt(2)-math.Sqrt(2))
 }
