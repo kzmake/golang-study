c72fbe8032982fcbf55a590309b712b4d4a7bc1c
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-09 22:28:30 +0900

    Implement Sqrt function with Newton's method

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-    var z float64 = 1.0
-    return z
+	var z float64 = 1.0
+
+	for i := 0; i < 10; i++ {
+		z = z - 0.5*(z*z-x)/z
+	}
+
+	return z
 }
 
 func main() {
-    fmt.Println("math.Sqrt(2) :\t", math.Sqrt(2))
-    fmt.Println("Sqrt(2)      :\t", Sqrt(2))
-    fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", (Sqrt(2) - math.Sqrt(2)))
+	fmt.Println("math.Sqrt(2) :\t", math.Sqrt(2))
+	fmt.Println("Sqrt(2)      :\t", Sqrt(2))
+	fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", Sqrt(2) - math.Sqrt(2))
 }
-
