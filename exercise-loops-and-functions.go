7b8bf7eb1957225fc802418e162d5828373b33a7
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-09 22:12:25 +0900

    Employ minor cosmetic changes to the outputs

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -11,8 +11,8 @@ func Sqrt(x float64) float64 {
 }
 
 func main() {
-    fmt.Println("math.Sqrt(2) :", math.Sqrt(2))
-    fmt.Println("Sqrt(2) :", Sqrt(2))
+    fmt.Println("math.Sqrt(2) :\t", math.Sqrt(2))
+    fmt.Println("Sqrt(2)      :\t", Sqrt(2))
     fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", (Sqrt(2) - math.Sqrt(2)))
 }
 
