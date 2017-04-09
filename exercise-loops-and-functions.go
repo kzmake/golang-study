bc6e73db89a43d83ce076ac19c294e4abf5a15fa
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-09 21:49:38 +0900

    Add math package to import for math.Sqrt()

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -2,12 +2,17 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
 func Sqrt(x float64) float64 {
+    var z float64 = 1.0
+    return z
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
+    fmt.Println("math.Sqrt(2) :", math.Sqrt(2))
+    fmt.Println("Sqrt(2) :", Sqrt(2))
+    fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", (Sqrt(2) - math.Sqrt(2)))
 }
 
