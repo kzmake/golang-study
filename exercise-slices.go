18b2ba4897b4f997ef6cc01025b31677d913ffde
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-11 02:01:44 +0900

    Prepare the 2-d array

--- exercise-slices.go
+++ exercise-slices.go
@@ -3,6 +3,12 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	s := make([][]uint8, dy)
+	for j := range s {
+		s[j] = make([]uint8, dx)
+	}
+
+	return s
 }
 
 func main() {
