2e4072bba7fd317333467fe19c1af865f3383dc6
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-11 23:07:03 +0900

    Implement WordCount function with map

--- exercise-maps.go
+++ exercise-maps.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"golang.org/x/tour/wc"
+    "strings"
 )
 
 func WordCount(s string) map[string]int {
 	m := map[string]int{}
 
 	for _, word := range strings.Fields(s) {
+		_, ok := m[word]
+
+		if ok {
+			m[word]++
+		} else {
+			m[word] = 1
+		}
 	}
 
 	return m
