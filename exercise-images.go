1b55f71fe2fd3458739fbe9ce0d1147505d0cd35
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-16 19:12:53 +0900

    Add Bounds function

--- exercise-images.go
+++ exercise-images.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"golang.org/x/tour/pic"
-
+	"image"
 	"image/color"
+
+	"golang.org/x/tour/pic"
 )
 
 type Image struct{}
 
 func (img Image) ColorModel() color.Model {
 	return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 256, 256)
 }
 
 func main() {
