6dbb7796f26de14a6fe8f7fbc2beedec3dff04c5
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-16 10:07:57 +0900

    Reflect review in exercise-loops-and-functions.go

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,32 +5,33 @@ import (
 	"math"
 )
 
-func isChecked(a float64, b float64) bool {
-	if math.Abs(a-b) < 1e-10 {
-		return true
+const (
+	CONVERGENCE_VALUE = 1e-10
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	is_checked := func(a, b float64) bool {
+		if math.Abs(a-b) < CONVERGENCE_VALUE {
+			return true
+		}
+		return false
 	}
 
-	return false
-}
-
-func Sqrt(x float64) (float64, int) {
-	var z float64 = 1.0
-
-	for i := 0; ; i++ {
-		_z := z
+	for {
+		pre_z := z
 		z = z - 0.5*(z*z-x)/z
 
-		if isChecked(z, _z) {
-			return z, i + 1
+		if is_checked(z, pre_z) {
+			return z
 		}
 	}
 }
 
 func main() {
 	msqrt := math.Sqrt(2)
-	sqrt, ite := Sqrt(2)
+	sqrt := Sqrt(2)
 
-	fmt.Println(ite, "Iterations")
 	fmt.Println("math.Sqrt(2) :\t", msqrt)
 	fmt.Println("Sqrt(2)      :\t", sqrt)
 	fmt.Println("  -> Error (Sqrt(2) - math.Sqrt(2)) :", sqrt-msqrt)
