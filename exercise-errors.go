74d80a2e82439de7c7e629634fe9243bd4ad5f0b
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-16 18:41:27 +0900

    Modify Sqrt function: return float64 -> (float64, error)

--- exercise-errors.go
+++ exercise-errors.go
@@ -12,11 +12,17 @@ const (
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
+	// %gは%vと違い、暗黙的にfloat64型へキャストされてるっぽいので無限ループにならない
+	// ただ、%g -> %v の変更の可能性と気持ち悪いので、float64(e)と明示的にキャスト
 	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
 }
 
-func Sqrt(x float64) float64 {
-	z := 1.0
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z := float64(1.0)
 	is_checked := func(a, b float64) bool {
 		if math.Abs(a-b) < CONVERGENCE_VALUE {
 			return true
@@ -29,12 +35,14 @@ func Sqrt(x float64) float64 {
 		z = z - 0.5*(z*z-x)/z
 
 		if is_checked(z, pre_z) {
-			return z
+			return z, nil
 		}
 	}
 }
 
 func main() {
 	fmt.Println(Sqrt(2))
+
+	// Error Tests
 	fmt.Println(Sqrt(-2))
 }
