d8ef91f5d99583cc90b9d92d6422d341d99ed377
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-04-16 19:23:39 +0900

    Add Test for Walk function: stdout outputs

--- exercise-equivalent-binary-trees.go
+++ exercise-equivalent-binary-trees.go
@@ -1,14 +1,28 @@
 package main
 
-import "golang.org/x/tour/tree"
+import (
+	"fmt"
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+	"golang.org/x/tour/tree"
+)
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Walk(t *tree.Tree, ch chan int) {
+	close(ch)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	return false
+}
 
 func main() {
+	// Test Walk
+	{
+		ch := make(chan int, 10)
+
+		go Walk(tree.New(2), ch)
+		fmt.Print("Walk(tree.New(1), ch) : ")
+		for v := range ch {
+			fmt.Print(v, " ")
+		}
+	}
 }
